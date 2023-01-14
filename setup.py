f7476c3f43731925ea5e686eaf95378c23b2b9ab
Austin Geary <austin.cg1@gmail.com>
2023-01-13 19:54:25 -0500

    Started EDA and cleaning

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle Competition to forecast microbusiness activity across the United States',
+    author='Austin Geary',
+    license='',
+)
